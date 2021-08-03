a28dab15cf7d8a8db4ab5d9d9f31fd999c3a1418
kim-shun <kimura1417080@gmail.com>
2021-08-03 23:06:40 +0900

    Animalクラスを継承してsetNameとsetAgeを呼び出す

--- src/example/Cat.java
+++ src/example/Cat.java
@@ -1,5 +1,10 @@
 package example;
 
-public class Cat {
-
+public class Cat extends Animal {
+	public Cat(String name, int age) {
+		setName(name);
+		setAge(age);
+		System.out.println("コンストラクター:Cat(String name, int age)が呼び出された");
+		System.out.println("  引数:name = " + name + ", age = " + age);
+	}
 }
