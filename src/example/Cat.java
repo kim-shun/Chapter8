75a11b8fa6567693e5ed00853cbaebf5a9214c45
kim-shun <kimura1417080@gmail.com>
2021-08-03 23:13:47 +0900

    Catクラスの定義

--- src/example/Cat.java
+++ src/example/Cat.java
@@ -7,4 +7,24 @@ public class Cat extends Animal {
 		System.out.println("コンストラクター:Cat(String name, int age)が呼び出された");
 		System.out.println("  引数:name = " + name + ", age = " + age);
 	}
+	
+	public void eat() {
+		eat("ご飯");
+	}
+	
+	public void eat(String food) {
+		printMessage(food + "を食べるよ！おいしいにゃー");
+		printMessage("お腹が一杯になったにゃー");
+		setHungry(false);
+	}
+	
+	public void playToy(String toy) {
+		printMessage(toy + "で遊ぶよ。楽しいにゃー");
+		printMessage("遊んでお腹が減ったにゃー");
+		setHungry(true);
+	}
+	
+	public void playToy() {
+		playToy("おもちゃ");
+	}
 }
