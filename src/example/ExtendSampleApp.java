0a5705a6890473f93e0bb6c9cb1dc8e6199c2535
kim-shun <kimura1417080@gmail.com>
2021-08-04 15:57:25 +0900

    インターフェースの実装を追加したクラスを利用する

--- src/example/ExtendSampleApp.java
+++ src/example/ExtendSampleApp.java
@@ -3,14 +3,14 @@ package example;
 public class ExtendSampleApp {
 
 	public static void main(String[] args) {
-		Cat tama = new Cat("タマ", 3);
+		Pet tama = new Cat("タマ", 3);
 		
 		tama.eat();
 		tama.introduceMyself();
 		tama.setAge(4);
 		tama.introduceMyself();
 
-		Dog pochi = new Dog("ポチ", 5);
+	    Pet pochi = new Dog("ポチ", 5);
 		
 		pochi.eat();
 		pochi.introduceMyself();
